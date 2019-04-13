5f9dbdf26ad9c78545b4d576591c411fa035f427
Max Caber <mcaber@cta.org>
2019-04-13 16:07:22 -0700

    back button goBack

--- src/BlogPage.tsx
+++ src/BlogPage.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { IonToolbar, IonTitle, IonContent, IonCard, IonHeader, IonCardHeader, IonCardTitle, IonCardSubtitle, } from '@ionic/react'
+import { IonToolbar, IonTitle, IonContent, IonCard, IonHeader, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButtons, IonBackButton, } from '@ionic/react'
+import { match } from 'react-router-dom';
 
-const BlogPage = ({history}) => (
+const BlogPage = ({ history, goBack }) => (
   <>
     <IonHeader>
       <IonToolbar color="primary">
+        <IonButtons slot="start">
+          <IonBackButton goBack={goBack} defaultHref='/' />
+        </IonButtons>
         <IonTitle>Blog Page</IonTitle>
       </IonToolbar>
     </IonHeader>
-    <IonContent onClick={(e) => {
-                        e.preventDefault();
-                        console.log('historys',history);
-                        history.goBack();
-                    }} >
+    <IonContent >
       <IonCard>
         <IonCardHeader>
           <IonCardSubtitle>Vue.js</IonCardSubtitle>
--- src/ItemDetailPage.tsx
+++ src/ItemDetailPage.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import { IonToolbar, IonTitle, IonContent, IonCard, IonHeader, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from '@ionic/react'
+import { IonToolbar, IonTitle, IonContent, IonCard, IonButtons, IonBackButton, IonHeader, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from '@ionic/react'
 
 
 
-const ItemDetailPage = ({match,history}) => (
+const ItemDetailPage = ({ match, history, goBack }) => (
     <>
         <IonHeader>
             <IonToolbar color="primary">
-                <IonTitle>Item Detail Page</IonTitle>
+                <IonButtons slot="start">
+                    <IonBackButton goBack={goBack} defaultHref='/items' />
+                </IonButtons>
+                <IonTitle>{match.params.id}</IonTitle>
             </IonToolbar>
         </IonHeader>
         <IonContent>
             <IonCard>
                 <IonCardHeader>
-                    <IonCardSubtitle>Home Page</IonCardSubtitle>
-                    <IonCardTitle>You chose {match.params.id} !!!</IonCardTitle>
+                    <IonCardSubtitle>A tasty fruit!</IonCardSubtitle>
+                    <IonCardTitle>{match.params.id}</IonCardTitle>
                 </IonCardHeader>
-                <IonCardContent>
-                    <IonButton onClick={(e) => {
-                        e.preventDefault();
-                        history.goBack();
-                    }} >Go Back To Items List</IonButton> 
-
-                </IonCardContent>
             </IonCard>
         </IonContent>
     </>
